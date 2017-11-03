4696105081359384147fb1b200b04373928281fa
skboyle <boyle.steven@gmail.com>
2017-11-03 15:03:48 -0400

    Heroku update

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,10 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+
+if['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
 
 module FeedbackLoop
   class Application < Rails::Application
