75baf646fcbe984b847e7ae9038326ec05cb771c
skboyle <boyle.steven@gmail.com>
2017-11-18 21:24:28 -0500

    Info Fix

--- app/javascript/react/src/InfoContainer.js
+++ app/javascript/react/src/InfoContainer.js
@@ -6,7 +6,7 @@ const InfoContainer = props => {
   return(
     <div id="info-tile" className="large-12 columns ">
       <h1>Welcome to Feedback Loop!</h1>
-      <p>Feedback Loop is a place for music makers to improve their song writing by giving a receiving constructive feedback.
+      <p>Feedback Loop is a place for music makers to improve their song writing by giving and receiving constructive feedback.
       <br/>
       <br/>
       After loging in though spotify you'll be brought to the 'Feed'. The feed is an album art gallery of the most recentally posted songs. From there you can click a track's album art to see it's page. Each song page includes the song's info along with a form for giving constructive feedback based on structure, mixdown and style and for giving Spotify recomendations. You'll also see the feedback the song as already received.
